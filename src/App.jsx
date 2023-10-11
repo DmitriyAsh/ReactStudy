ff8e7f2ae5780dde33959a41e40c58dbc0c5069d
Dmitriy Dmitriev <dmitriyd1107@gmail.com>
2023-10-11 18:34:16 +0700

    add hook useFetching

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import { usePosts } from "./hooks/usePosts";
 import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
     // useState возвращает массив из 2х объектов, первый это само состояние (posts), второй- функция, которое это состояние изменятет (setPosts)
@@ -21,7 +22,10 @@ function App() {
     const [modal, setModal] = useState(false); // Состояния для модального окна, отвечающиего за его видимость
     const [filter, setFilter] = useState({ sort: "", query: "" }); // Состояние для компонента PostFilter
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-    const [isPostsLoading, setIsPostsLoading] = useState(false);
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    });
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -32,15 +36,6 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter((p) => p.id !== post.id));
     };
-
-    async function fetchPosts() {
-        setIsPostsLoading(true);
-        setTimeout(async () => {
-            const posts = await PostService.getAll();
-            setPosts(posts);
-            setIsPostsLoading(false);
-        }, 1000);
-    }
 
     useEffect(() => {
         fetchPosts();
@@ -56,6 +51,7 @@ function App() {
             </MyModal>
             <hr style={{ margin: "15px 0" }} />
             <PostFilter filter={filter} setFilter={setFilter} />
+            {postError && <h1>An error has occurred ${postError}</h1>}
             {isPostsLoading ? (
                 <div
                     style={{
