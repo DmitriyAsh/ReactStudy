da271d9f36b99b5a000f900e35350d50993ea55d
Dmitriy Dmitriev <dmitriyd1107@gmail.com>
2023-10-19 16:47:24 +0700

    change routing

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,16 +4,15 @@ import Error from "../pages/Error";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
 import PostIdPage from "../pages/PostIdPage";
+import { routes } from "../router/routes";
 
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path='/posts/:id' element={<PostIdPage />} />
-            <Route path='/' element={<Posts />} />
-            <Route path='/error' element={<Error />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/posts' element={<Posts />} />
-            <Route path='/*' element={<Navigate to='/error' replace />} />
+            {routes.map((route) => (
+                <Route path={route.path} element={<route.element />} />
+            ))}
+            <Route path='/*' element={<Navigate to='/posts' replace />} />
         </Routes>
     );
 };
